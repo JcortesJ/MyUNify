e274afcbbc570b365cefd8cdfbb04bf7766a79e0
SimonRamos88 <joramos@unal.edu.co>
2022-02-02 20:47:06 -0500

    Pantallas del login y del inicio

---
+++ lib/widgets/Colores.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Colores{
+
+  static Color colorNaranja = Color(0xFFFAAD80);
+  static Color colorRosado = Color(0xFFFF6767);
+  static Color colorComplementario = Color(0xffFF3D68);
+}
---
+++ lib/widgets/login_widgets/Containers.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../Colores.dart';
+
+class Containers extends StatelessWidget {
+  
+  late Widget child; 
+
+  Containers({ Key? key, @required child }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+      decoration: BoxDecoration(
+        borderRadius:  BorderRadius.circular(29),
+        color: Colores.colorComplementario,                   
+      ),
+      width: size.width * 0.9,
+      //height: 80,
+      child:child
+    );
+  }
+}
